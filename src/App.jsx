5b3be8212031ebe0d9aaab6702c23c9774b6d2f7
ttnguyen115 <thanhtrung1012000@gmail.com>
2023-12-06 21:01:16 +0700

    install mui and config eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Button } from '@mui/material'
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarms'
+import ThreeDRotationIcon from '@mui/icons-material/ThreeDRotation'
+import { pink } from '@mui/material/colors'
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>TrungNT102</div>
+      <Button variant="text">Hello World 1</Button>
+      <Button variant="contained">Hello World 2</Button>
+      <Button variant="outlined">Hello World 3</Button>
+      <AccessAlarmIcon sx={{ color: pink[500] }} />
+      <ThreeDRotationIcon />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
